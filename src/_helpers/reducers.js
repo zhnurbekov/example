0f55c09617b8a50eb35b21a3012b494ef51054e2
Jangle <n.zhangeldy@gmail.com>
2019-04-16 17:56:30 +0600

    Подправил ошибки архитектуры

--- src/_helpers/reducers.js
+++ src/_helpers/reducers.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import locale from '../components/LocaleProvider/LocaleDucks';
+import localeReducer, { module as locale } from '../components/LocaleProvider/LocaleDucks';
 import { connectRouter } from 'connected-react-router'
 
 export default history => combineReducers({
   router: connectRouter(history),
-  locale
+  [locale]: localeReducer
 });
